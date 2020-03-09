e0e88ba1be42a087e0e4d7d82156fc528014af82
Luciano Rolando Lorenti <lucianolorenti@gmail.com>
2020-03-09 18:48:18 +0100

    Add my name

--- tsx/index.tsx
+++ tsx/index.tsx
@@ -29,7 +29,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Luciano Lorenti
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
